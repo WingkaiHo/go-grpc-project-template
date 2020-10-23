b0ee92caa385efe36f1a83300d4e45d2f206298b
yongjiahe <yongjiahe@tuputech.com>
2020-10-23 14:52:35 +0800

    feat(main): print config

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"log"
+
 	"github.com/WingkaiHo/go-grpc-project-template/app"
 	"github.com/WingkaiHo/go-grpc-project-template/config"
 )
 
 func main() {
-	app.StartApp(config.GetConfigByEnv())
+	cfg := config.GetConfigByEnv()
+	log.Println(cfg)
+	app.StartApp(cfg)
 }
