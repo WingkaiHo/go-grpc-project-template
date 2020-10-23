6b61a41f8ef6a582b2ab625e5a4bd135736d30db
yongjiahe <yongjiahe@tuputech.com>
2020-10-23 12:35:26 +0800

    feat(config): demo

---
+++ config/config.go
@@ -0,0 +1,15 @@
+package cfg
+
+// AppConfig is app config
+type AppConfig struct {
+	App  string
+	Port int
+	Env  string
+}
+
+// Config values
+var Config = &AppConfig{
+	App:  "go-grpc-project-template",
+	Port: 55006,
+	Env:  "development",
+}
